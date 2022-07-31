9462b7d300cc89420b24e4fc0989d16cff7ef33d
li494391537 <lirui494391537@outlook.com>
2022-07-31 12:19:53 +0800

    init

---
+++ src/main/kotlin/com/github/gyangustar/zhaicaipuserver/ZhaicaipuServerApplication.kt
@@ -0,0 +1,11 @@
+package com.github.gyangustar.zhaicaipuserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZhaicaipuServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ZhaicaipuServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/gyangustar/zhaicaipuserver/ZhaicaipuServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.gyangustar.zhaicaipuserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZhaicaipuServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
