63a83824f48943dbfe0a8427881a4a8e72cb26bb
li494391537 <lirui494391537@outlook.com>
2022-08-08 00:35:41 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,23 +17,22 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-graphql")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.springframework.session:spring-session-data-redis")
-    runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework:spring-webflux")
     testImplementation("org.springframework.amqp:spring-rabbit-test")
     testImplementation("org.springframework.graphql:spring-graphql-test")
 }
+
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
--- src/main/kotlin/com/github/gyangustar/zhaicaipuserver/ZhaicaipuServerApplication.kt
+++ src/main/kotlin/com/github/gyangustar/zhaicaipuserver/ZhaicaipuServerApplication.kt
@@ -1,11 +1,68 @@
 package com.github.gyangustar.zhaicaipuserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
+import org.springframework.security.oauth2.core.OAuth2TokenValidator
+import org.springframework.security.oauth2.jwt.*
+import org.springframework.security.oauth2.jwt.JwtClaimNames.AUD
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
-@SpringBootApplication(exclude=[MongoAutoConfiguration::class, MongoDataAutoConfiguration::class])
+
+@SpringBootApplication
 class ZhaicaipuServerApplication
 
 fun main(args: Array<String>) {
     runApplication<ZhaicaipuServerApplication>(*args)
+}
+@RestController
+class TestController {
+    @RequestMapping("/ping")
+    fun pingController() : String {
+        return "pong"
+    }
+    @GetMapping("/user")
+    fun user(@AuthenticationPrincipal principal: Jwt): Map<String, Any>{
+        return principal.claims
+    }
+}
+
+@EnableWebSecurity
+class SecurityConfiguration {
+    @Bean
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http .
+            authorizeRequests {
+                it
+                    .antMatchers("/ping").permitAll()
+                    .anyRequest().authenticated()
+            }.
+            oauth2ResourceServer {
+                it.jwt { }
+            }
+
+        return http.build()
+    }
+    @Bean
+    fun jwtDecoder(): JwtDecoder {
+        val jwtDecoder: NimbusJwtDecoder = JwtDecoders.fromIssuerLocation("https://account.kuroneko.xyz:8443") as NimbusJwtDecoder
+        val audienceValidator = audienceValidator()
+        val withIssuer: OAuth2TokenValidator<Jwt> = JwtValidators.createDefaultWithIssuer("https://account.kuroneko.xyz:8443")
+        val withAudience: OAuth2TokenValidator<Jwt> = DelegatingOAuth2TokenValidator(withIssuer, audienceValidator)
+        jwtDecoder.setJwtValidator(withAudience)
+        return jwtDecoder
+    }
+    fun audienceValidator(): OAuth2TokenValidator<Jwt?> {
+        return JwtClaimValidator<List<String>>(AUD) { aud -> aud.contains("kuancaipu") }
+    }
 }
